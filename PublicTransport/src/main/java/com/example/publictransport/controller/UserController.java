dc165e788f70571b0947955f61041345e37e80e1
pernillamoreno <113672077+pernillamoreno@users.noreply.github.com>
2023-03-03 11:46:40 +0100

    Delete PublicTransport directory

--- PublicTransport/src/main/java/com/example/publictransport/controller/UserController.java
+++
@@ -1,4 +0,0 @@
-package com.example.publictransport.controller;
-
-public class UserController {
-}
--- PublicTransport/src/main/java/com/example/publictransport/exeption/RouteExeption.java
+++
@@ -1,8 +0,0 @@
-package com.example.publictransport.exeption;
-
-public class RouteExeption extends RuntimeException  {
-
-    public RouteExeption (Long id){
-        super("This Route was not find: " + id);
-    }
-}
